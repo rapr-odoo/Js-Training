82728dd7fdcc98255f26d1cc6da1e7bcb84886e6
Rahul <rapr@odoo.com>
2022-02-10 15:24:08 +0530

    Added Custom Event Types Added custom Event Handlers, Event Listeners Added Custom Events

--- task1.js
+++ task1.js
@@ -1,5 +1,5 @@
 var flag = 0
-const OnClickEvent = () => alert("You Clicked Me!!!");
+const OnClickEvent = () => document.getElementById('h3text').dispatchEvent(new Event(EVENT_TYPE));
 const OnOver = () => {
   flag = 0;
   document.getElementById('demo').innerHTML = "You are hovering button !!!";
@@ -18,29 +18,34 @@ const loadPromise = () => {
       if (document.readyState == 'complete') {
         console.log(document.readyState);
         const h1 = document.getElementById('h1text');
-        h1.addEventListener('mouseover', cusClick(document.getElementById('h1text')));
         resolve('done');
       }
     }
   });
 }
 
-const cusOut = (h1) => {
-  h1.style = "color: black";}
+const cusOut = (ev) => ev.target.style = "color: black";
+const cusOver = (ev) => ev.target.style = "color: red;";
 
-const cusOver = (h1) => {
-  console.log(h1);
-  h1.style = "color: red;";}
+const EVENT_TYPE = "rahul";
+document.getElementById('h3text').addEventListener(EVENT_TYPE, (event) => event.target.innerHTML = "Called");
 
-h1 = document.getElementById('h1text')
-h1.addEventListener('mouseover', cusOver(h1))
-h1.addEventListener('mouseout', cusOut(h1))
 
-/*(async function() {
+// h1 = document.getElementById('h1text')
+// h1.addEventListener('mouseover', cusOver)
+// h1.addEventListener('mouseout', cusOut)
+
+(async function() {
   const res = await loadPromise();
   console.log(res, 'Done')
   var h1 = document.getElementById('h1text');
-  h1.innerHTML = "New H1"
+  h1.addEventListener('mouseover', cusOver)
+  h1.addEventListener('mouseout', cusOut)
+  h1.addEventListener('click', () => alert('You clicked Custom Buton!!!'));
   var h2 = document.getElementById('h2text');
   //h1.addEventListener('mouseover', cusClick(h1))
-})();*/
+
+  const cusOverEvent = new Event('mouseover', {bubbles: true, cancelable: true});
+  h2.addEventListener('click', () => alert('You clicked Custom H2 Buton!!!'));
+  h2.dispatchEvent(cusOverEvent);
+})();
